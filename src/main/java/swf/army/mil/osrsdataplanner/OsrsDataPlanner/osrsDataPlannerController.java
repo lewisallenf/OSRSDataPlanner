adeca83d6314dd1fbc5cbcab7188d23b65463269
Lewis Allen <lewis.f.allen.mil@army.mil>
2025-10-20 15:42:26 -0500

    first commit

---
+++ src/main/java/swf/army/mil/osrsdataplanner/OsrsDataPlanner/osrsDataPlannerController.java
@@ -0,0 +1,4 @@
+package swf.army.mil.osrsdataplanner.OsrsDataPlanner;
+
+public class osrsDataPlannerController {
+}
---
+++ src/test/java/swf/army/mil/osrsdataplanner/OsrsDataPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package swf.army.mil.osrsdataplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrsDataPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
